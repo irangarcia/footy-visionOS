b6a72758ef3a95b91fbe05055af0cb0e49cb9ade
Iran Garcia <irangarciaj@gmail.com>
2023-08-04 20:26:47 -0300

    Initial Commit

---
+++ Footy/FootyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootyApp.swift
+//  Footy
+//
+//  Created by Iran Garcia Junior on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FootyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
